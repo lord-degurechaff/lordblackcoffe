bb7f91982119f79bd5fcea89fb262f1d5bbcadfd
lord-degurechaff <84936975+lord-degurechaff@users.noreply.github.com>
2021-10-04 02:51:44 +0700

    Update group.js

--- message/group.js
+++ message/group.js
@@ -38,7 +38,7 @@ module.exports = welcome = async (dha, anu) => {
                 let v = dha.contacts[num] || { notify: num.replace(/@.+/, '') }
                 anu_user = v.vname || v.notify || num.split('@')[0]
                 time_wel = moment.tz('Asia/Jakarta').format("HH:mm")
-                teks = `Halo Pendatang baru\n${anu_user} \nINTRO DULULAH BIAR KENAL\nNama : \nUmur :\nGender : \nAsal :\n\nSemoga Betah dan jangan lupa isi\nAnd Following Rules Group`
+                teks = `Waah Makhluk baru\n${anu_user} \nINTRO DULULAH BIAR KENAL\nNama : \nUmur :\nGender : \nAsal :\n\nSemoga Betah dan jangan lupa isi\nAnd Following Rules Group`
 	            buff = await getBuffer(`http://hadi-api.herokuapp.com/api/card/welcome?nama=${anu_user}&descriminator=${time_wel}&memcount=${memeg}&gcname=${encodeURI(mdata.subject)}&pp=${pp_user}&bg=https://i.postimg.cc/rFkw8MpX/IMG-20210807-151325.jpg`)
                 buttons = [{buttonId: `#infogrup`,buttonText:{displayText: 'SELAMAT DATANG'},type:1}]
                 imageMsg = (await dha.prepareMessageMedia((buff), 'imageMessage', {thumbnail: buff})).imageMessage
@@ -56,7 +56,7 @@ module.exports = welcome = async (dha, anu) => {
                 memeg = mdata.participants.length
                 out = `Jangan Lupakan Kenangan kita\n${anu_user}\nSemoga kamu bahagia diluar sana`
                 buff = await getBuffer(`http://hadi-api.herokuapp.com/api/card/goodbye?nama=${anu_user}&descriminator=${time_wel}&memcount=${memeg}&gcname=${encodeURI(mdata.subject)}&pp=${pp_user}&bg=https://i.postimg.cc/rFkw8MpX/IMG-20210807-151325.jpg`)
-                buttons = [{buttonId: `#alquran 1`,buttonText:{displayText: 'SELAMAT JALAN'},type:1}]
+                buttons = [{buttonId: `#alquran 1`,buttonText:{displayText: 'Sayonara'},type:1}]
                 imageMsg = (await dha.prepareMessageMedia((buff), 'imageMessage', {thumbnail: buff})).imageMessage
                 buttonsMessage = { contentText: `${out}`, footerText: 'BALIK LAGI BELIKAN AYAM BAKAR', imageMessage: imageMsg, buttons: buttons, headerType: 4 }
                 prep = await dha.prepareMessageFromContent(mdata.id,{buttonsMessage},{})
